68dcb7bc74acc5be4be82aebe8b51db094ce71dd
Deepanshu Kesharwani <120784215+Deepanshu-kesharwani@users.noreply.github.com>
2025-01-18 23:13:54 +0530

    code file

---
+++ audio_extract.py
@@ -0,0 +1,18 @@
+from moviepy.editor import VideoFileClip
+
+try:
+    # Open the video file
+    video = VideoFileClip("Video.mp4")
+
+    # Extract the audio
+    audio = video.audio
+
+    # Write the audio to MP3
+    audio.write_audiofile("audio_Extracted.mp3")
+
+    # Clean up by closing the files
+    video.close()
+    audio.close()
+
+except Exception as e:
+    print(f"An error occurred: {e}")
